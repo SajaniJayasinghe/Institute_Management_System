4ca80acb3a5d0a01572199e0319cdc9abb4e60f8
Sajani Jayasinghe <jayasinghesajani98@gmail.com>
2022-08-24 16:23:01 +0530

    backend packages install

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+//app middleware
+app.use(bodyParser.json());
+// app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 8070;
+
+//To accept the JSON Data
+app.use(express.json());
+
+const URL = process.env.MONGODB_URL;
+process.env.SUPPRESS_NO_CONFIG_WARNING = 'y';
+
+mongoose.connect(URL, {
+    //useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useFindAndModify: false
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+console.log("Mongodb connection success!!!");
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number: ${PORT}`)
+})
