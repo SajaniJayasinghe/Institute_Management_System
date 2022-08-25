c2fb82d6567fadf726c05b900f79a3dbddf9c415
Sajani Jayasinghe <jayasinghesajani98@gmail.com>
2022-08-25 16:00:46 +0530

    frontend creation

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+export default class App extends Component {
+    render() {
+        return (
+          <Router>
+            <Switch>
+            
+             {/* Sajani */}
+
+
+
+             {/* Randy */}
+
+
+
+             {/* Aroshini */}
+
+
+             {/* Imaya */}
+
+
+            </Switch>
+        </Router>
+
+      )
+    }
+}
