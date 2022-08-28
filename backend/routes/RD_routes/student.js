f5246bd5a3e13d4c171905c11fdb0d433e067e93
Randima Dilshani <randimadilshani98@gmail.com>
2022-08-28 22:46:50 +0530

    Student signin, profile update, delete

--- backend/routes/RD_routes/student.js
+++ backend/routes/RD_routes/student.js
@@ -58,4 +58,78 @@ router.post("/student/signup",async (req, res) => {
     }
   });
 
+//get student profile
+router.get("/student/profile", auth, async (req, res) => {
+    try {
+      res.status(201)
+      res.send({ success: "Student Logged In", Stu: req.Stu });
+    } 
+      catch (error) {
+      res.status(500)
+      res.send({ status: "Error with profile", error: error.message });
+    }
+  });
+
+  
+  //log out profile
+  router.post("/student/logout", auth, async (req, res) => {
+    try {
+      req.Stu.tokens = req.Stu.tokens.filter((token) => {
+        return token.token !== req.token;
+      });
+      await req.Stu.save();
+      res.status(200).send("Logout successfully");
+    } catch (error) {
+      res.status(500).send(error.message);
+      console.log(error.message);
+    }   
+ });
+
+// update student profile
+router.put('/student/update', auth, async (req, res) => {
+    try {
+      const {
+          studentName,
+          phone,
+          email,
+        } = req.body;
+  
+      let Stu = await student.findOne({email})
+        
+      if (!Stu) {
+          throw new Error('There is no student account')
+        }
+  
+        const studentUpdate = await student.findByIdAndUpdate(req.stu.id, 
+          {
+            studentName:studentName,
+            phone:phone,
+            email:email
+          })
+  
+          res.status(200).send({status: 'Student Profile Updated', Stu: studentUpdate})
+  
+        } catch (error) {
+          res.status(500).send({error: error.message})
+          console.log(error)
+        }
+      });
+  
+      //delete student account
+    router.delete("/student/delete", auth, async (req, res) => {
+        try {
+          const Stu = await student.findById(req.Stu.id);
+          if (!Stu) {
+            throw new Error("There is no student to delete");
+          }
+          const deleteProfile = await student.findByIdAndDelete(req.stu.id);
+          res.status(200).send({ status: "Student deleted", Stu: deleteProfile });
+        } catch (error) {
+          res
+            .status(500)
+            .send({ status: "error with /delete/:id", error: error.message });
+        }
+      });
+
+
   module.exports = router;
