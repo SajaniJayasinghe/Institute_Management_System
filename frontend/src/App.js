35bc9460f2b378e66e4c8f60b081ef0aeb8c402a
Aroshini88 <arodeez88@gmail.com>
2022-09-03 12:17:02 +0530

    Merge branch 'main' of https://github.com/SajaniJayasinghe/Institute_Management_System

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,37 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import CreateNewCourses from './components/SS_Components/CreateNewCourses';
-import CourseDetails from './components/SS_Components/CourseDetails';
-import UpdateCourseDetails from './components/SS_Components/UpdateCourseDetails';
-import SpecificCourse from './components/SS_Components/SpecificCourse';
-import CoursesDisplay from './components/SS_Components/CoursesDisplay';
-import AdminDashboard from './components/SS_Components/adminDashboard';
-import HomePage from './components/SS_Components/HomePage';
+import CreateNewCourses from "./components/SS_Components/CreateNewCourses";
+import CourseDetails from "./components/SS_Components/CourseDetails";
+import UpdateCourseDetails from "./components/SS_Components/UpdateCourseDetails";
+import SpecificCourse from "./components/SS_Components/SpecificCourse";
+import CoursesDisplay from "./components/SS_Components/CoursesDisplay";
+import AdminDashboard from "./components/SS_Components/adminDashboard";
+import HomePage from "./components/SS_Components/HomePage";
+import CreateFeedback from "./components/AA_Components/create-feedback.component";
 
 export default class App extends Component {
-    render() {
-        return (
-          <Router>
-           <Routes>
-            
-             {/* Sajani */}
-             <Route path = "/" element={<HomePage/>}exact/>
-             <Route path = "/createCourse" element= {<CreateNewCourses/>} />
-             <Route path = "/courseDetails" element={<CourseDetails/>}/>
-             <Route path = "/update/:courseID" element={<UpdateCourseDetails/>}/>
-             <Route path = "/specific/:courseID" element={<SpecificCourse/>}/>
-             <Route path = "/coursesdisplay" element={<CoursesDisplay/>}/>
-             <Route path = "/admindashboard" element={<AdminDashboard/>}/>
-            
+  render() {
+    return (
+      <Router>
+        <Routes>
+          {/* Sajani */}
+          <Route path="/createCourse" element={<CreateNewCourses />} exact />
+          <Route path="/courseDetails" element={<CourseDetails />} />
+          <Route path="/update/:courseID" element={<UpdateCourseDetails />} />
+          <Route path="/specific/:courseID" element={<SpecificCourse />} />
+          <Route path="/coursesdisplay" element={<CoursesDisplay />} />
+          <Route path="/admindashboard" element={<AdminDashboard />} />
+          <Route path="/" element={<HomePage />} />
 
-             
-             {/* Randy */}
+          {/* Randy */}
 
+          {/* Aroshini */}
+          <Route path="/specific/:courseID" element={<CreateFeedback />} />
 
-
-             {/* Aroshini */}
-
-
-             {/* Imaya */}
-
-
-            </Routes>
-        </Router>
-
-      )
-    }
+          {/* Imaya */}
+        </Routes>
+      </Router>
+    );
+  }
 }
-
